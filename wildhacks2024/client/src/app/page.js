0e7302efbb711af6da0a4f3371bda0ebb73a5b13
yongssh <yongyu.yy.huang@gmail.com>
2024-04-06 18:06:39 -0500

    message communication on localhost works, finished tutorial

--- wildhacks2024/client/src/app/page.js
+++ wildhacks2024/client/src/app/page.js
@@ -1,26 +1,42 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import io from 'socket.io-client'
-
+import {useEffect, useState} from 'react';
 
 const socket = io.connect("http://localhost:3001")
 
 function Home() {
 
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
   const sendMessage = () => {
-    socket.emit('message', 'Hello from client');
+    //console.log('Message sent');
+    socket.emit('send_message', {message });
   };
 
+  
+  useEffect(() => {
+  socket.on("receive_message", (data) => {
+    setMessageReceived(data.message);
+  });
+  }, [socket])
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main>
+      <div>
         <p>
-          WildHacks 2024 Timer Project
+         Bearly -- A Group Pomodoro Timer 
         </p>
       </div>
-      <div className="buttonTest">
-        <input placeholder="send message"/>
+      <div>
+        <input placeholder="send message" onChange={(event) =>{
+          setMessage(event.target.value);
+        }}
+          />
         <button onClick={sendMessage}> Send Message </button>
+        <h1> Message:</h1>
+        {messageReceived}
       </div>
       
     </main>
--- wildhacks2024/server/index.js
+++ wildhacks2024/server/index.js
@@ -1,24 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const http = require("http");
-const {Server} = require('socket.io');
+const {Server} = require("socket.io");
 
 const cors = require("cors") 
+
 app.use(cors());
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 const io = new Server(server, {
     cors:{
         origin: "http://localhost:3000",
         methods:["GET", "POST"],
     },
-
 });
 
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
+
+    socket.on("send_message", (data) =>{
+        socket.broadcast.emit("receive_message", data);
+    });
 });
+
+//User Connected: 48BvFCeVv_uZN8YgAAAX
 
 server.listen(3001, () => {
     console.log("SERVER IS RUNNING");
