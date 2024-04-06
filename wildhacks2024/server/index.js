3d66c412a108dcce9eadce25d28139a589ddd017
yongssh <yongyu.yy.huang@gmail.com>
2024-04-06 16:50:49 -0500

    socketio things

--- wildhacks2024/server/index.js
+++ wildhacks2024/server/index.js
@@ -1,1 +1,25 @@
-// server.js
+const express = require('express');
+const app = express();
+const http = require("http");
+const {Server} = require('socket.io');
+
+const cors = require("cors") 
+app.use(cors());
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors:{
+        origin: "http://localhost:3000",
+        methods:["GET", "POST"],
+    },
+
+});
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+});
+
+server.listen(3001, () => {
+    console.log("SERVER IS RUNNING");
+});
