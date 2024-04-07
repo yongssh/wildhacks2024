141b5b31caab09a563e9ab574b70992d0ff38d59
caterose <146782431+caterose@users.noreply.github.com>
2024-04-06 21:52:44 -0500

    timer with user input and start countdown

--- wildhacks2024/src/components/Timer.js
+++ wildhacks2024/src/components/Timer.js
@@ -1,39 +1,56 @@
 import React, {useState, useEffect} from "react";
+//https://www.geeksforgeeks.org/how-to-create-popup-box-in-reactjs/
 
 export default function Timer() {
     const [minutes, setMinutes] = useState(0);
-    const [seconds, setSeconds] = useState(5);
+    const [seconds, setSeconds] = useState(0);
     const [displayMessage, setDisplayMessage] = useState(false);
+    const [userInput, setUserInput] = useState('');
+
+    const handleInputChange = (event) => {
+        setUserInput(event.target.value);
+    };
+
+    const startCountdown = () => {
+        let inputMinutes = parseInt(userInput, 10);
+
+        if (inputMinutes <= 0 || inputMinutes > 120) {
+            setDisplayMessage(true);
+
+        } else {
+            setDisplayMessage(false);
+            setMinutes(inputMinutes); 
+            setSeconds(0);
+        }
+        
+
+    };
 
     useEffect(() => {
         let interval = setInterval(() => {
-            clearInterval(interval);
 
             if (seconds === 0) {
                 if (minutes !== 0) {
                     setSeconds(59);
                     setMinutes(minutes - 1);
-                } else {
-                    let minutes = displayMessage ? 24 : 4;
-                    let seconds = 59;
-
-                    setSeconds(seconds);
-                    setMinutes(minutes);
-                    setDisplayMessage(!displayMessage);
-                }
+                } 
             } else {
                 setSeconds(seconds - 1);
             }
         }, 1000);
-    }, [seconds]);
+
+        return () => clearInterval(interval);
+    }, [seconds, minutes, displayMessage]); 
 
     const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
     const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
     return (
         <div className="timer">
+            <input type="number" value={userInput} onChange={handleInputChange} />
+            <button className="startBtn" onClick={startCountdown}>Start Countdown</button>
             <div className="message">
-                {displayMessage && <div>Break time! New session starts in: </div> }
+                {displayMessage && <div>please input a whole number between 1 and 120 </div> }
             </div>
 
             <div className="timer">
