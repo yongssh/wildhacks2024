3ba3e94b90e7db9cb0e7e7ffe73f482a5a1d7b75
yongssh <yongyu.yy.huang@gmail.com>
2024-04-07 11:42:59 -0500

    fiddling w css, moved bears to components

--- wildhacks2024/server/index.js
+++ wildhacks2024/server/index.js
@@ -1,4 +1,22 @@
 // Server-side logic for room-specific timers
+const express = require("express");
+const app = express();
+const http = require("http");
+const {Server} = require("socket.io");
+
+const cors = require("cors") 
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors:{
+        origin: "http://localhost:3000",
+        methods:["GET", "POST"],
+    },
+});
+
 const roomTimers = {};
 
 io.on("connection", (socket) => {
